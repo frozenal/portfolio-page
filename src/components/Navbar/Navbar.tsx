5cf591a9636c753ad8ed9ebd1c28a7cb0ee464a7
Joshua Chisolm <jchisolm@Joshuas-MacBook-Pro.local>
2020-11-29 22:02:20 -0500

    make navbar responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ interface Props {}
 
 const Navbar = (props: Props) => {
   return (
-    <div className="fixed top-0 flex flex-row justify-between w-screen py-3 px-3 border border-b-1 border-opacity-20 border-gray-600 bg-white mb-14 items-center z-10 ">
+    <div className="fixed top-0 flex md:flex-row flex-col justify-between w-screen py-3 px-3 border border-b-1 border-opacity-20 border-gray-600 bg-white mb-14 items-center z-10 ">
       <h1 className="text-2xl border-b-4 border-purple-600 border-opacity-75">
         Joshua Chisolm
       </h1>
